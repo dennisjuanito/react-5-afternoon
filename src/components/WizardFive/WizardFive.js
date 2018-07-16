3fc58057a3c1eafc7f54e59d41d32c360f8b6b28
dennis juanito <dennisjuanito@gmail.com>
2018-07-15 21:13:07 -0600

    almost finished

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,22 +1,31 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {updateRealEstateAgent} from "../../ducks/reducer";
+import {connect} from "react-redux";
 
 class WizardFive extends Component {
-
+   
     render(){
+        let {updateRealEstateAgent} = this.props;
         return(
             <div className="parent-div">
                     <div className="vert-align">    
 
                     <p>Are you currently working with a real estate agent?</p> <br />
                     <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
+                        <Link to="/wSix"><button onClick={() => updateRealEstateAgent("true")}>Yes</button></Link>
+                        <Link to="/wSix"><button onClick={() => updateRealEstateAgent("false")}>No </button></Link>
                     </div>
                 </div>
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    let {realEstateAgent} = state;
+    return {
+        realEstateAgent: state.realEstateAgent
+    }
+}
 
-export default WizardFive;
+export default connect(mapStateToProps, {updateRealEstateAgent})(WizardFive);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { HashRouter as Router } from "react-router-dom";
+import {Provider }from "react-redux";
+import store from "./store.js";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
+// registerServiceWorker();
